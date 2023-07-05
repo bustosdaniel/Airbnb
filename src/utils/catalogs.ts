fc2cae9ccccdf508ac4365f719530502d2e6ea7d
daniel <bustosneifer@gmail.com>
2023-07-05 12:35:47 -0500

    added catalogs.ts

---
+++ src/utils/catalogs.ts
@@ -0,0 +1,76 @@
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+export const information = [
+  {
+    id: 1,
+    img: 'https://independenttravelcats.com/wp-content/uploads/2013/08/Airbnb.jpg',
+    host: 'SUPER HOST',
+    title: 'Entire apartment. 2 beds',
+    rating: '4.40',
+    subTitle: 'Stylist apartment in center of the city'
+  },
+  {
+    id: 2,
+    img: 'https://media.wired.com/photos/59331c9f4cd5ce6f96c0c5a1/3:2/w_1600%2Cc_limit/airbnb.jpg',
+    host: '',
+    title: 'Private room',
+    rating: '4.25',
+    subTitle: 'Cozy, peaceful and private room with...'
+  },
+  {
+    id: 3,
+    img: 'https://beachlifebliss.com/wp-content/uploads/2021/06/Seaspray-Oceanfront-Bedroom-Vacation-Rental.jpg',
+    host: '',
+    title: 'Entire house',
+    rating: '4.96',
+    subTitle: 'Mordern House in a remote area'
+  },
+  {
+    id: 4,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ5ifOCLoh0vYOHf6MSxxERGNUJZG_bamDpQ&usqp=CAU',
+    host: 'SUPER HOST',
+    title: 'Entire apartment. 2 beds',
+    rating: '4.85',
+    subTitle: 'Stylist room in design district '
+  },
+  {
+    id: 5,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEQYHNYbC5NAPhI56RRGrfBzevVwYHiDSS4g&usqp=CAU',
+    host: '',
+    title: 'Private room',
+    rating: '4.54',
+    subTitle: 'Modern apartment close to nature'
+  },
+  {
+    id: 6,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkwwMGKjdZVcGlwrj6g0OuhnoUzV3-s6oYMQ&usqp=CAU',
+    host: '',
+    title: 'Entire house',
+    rating: '4.64',
+    subTitle: 'House in a remote area'
+  }
+]
+
+export const location = [
+  {
+    id: 1,
+    icon: LocationOnIcon,
+    name: 'Helsinki, Finland'
+  },
+  {
+    id: 2,
+    icon: LocationOnIcon,
+    name: 'Turku, Finland'
+  },
+  {
+    id: 3,
+    icon: LocationOnIcon,
+    name: 'Oulu, Finland'
+  },
+  {
+    id: 4,
+    icon: LocationOnIcon,
+    name: 'Vaasa, Finland'
+  }
+]
+
